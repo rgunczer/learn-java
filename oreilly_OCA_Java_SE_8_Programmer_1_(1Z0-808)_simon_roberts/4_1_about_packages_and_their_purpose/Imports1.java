19d96a6dd80466fb848aaee9bfa4e0df16125f77
NITRO\flan <rgunczer@gmail.com>
2020-11-28 23:10:14 +0100

    add imports test and hello world

---
+++ oreilly_OCA_Java_SE_8_Programmer_1_(1Z0-808)_simon_roberts/4_1_about_packages_and_their_purpose/Imports1.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Imports1 {
+    public static void main(String[] args) {
+        // System.out.println("Today is " + new java.util.Date());
+        System.out.println("TodayZ is " + new Date());
+    }
+}
